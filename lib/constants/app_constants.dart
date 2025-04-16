e6dfcffcb72319d326ec7c7cf8fff029f555c32a
Mohammad Mohammad <m.mohammad@TRAKLINK.local>
2025-04-16 19:05:04 +0300

    Initial commit

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,8 @@
+abstract class ThingsboardAppConstants {
+  static const thingsBoardApiEndpoint = 'http://77.245.2.171:8080';
+  static const thingsboardOAuth2CallbackUrlScheme =
+      'org.thingsboard.pe.app.auth';
+
+  static const thingsboardIOSAppSecret = 'RFQ0UnVta3dKYnZQdHp3YnV0OVdJOGZOajNJQms3eTFQRGFOTENNRjg3MVNVNXRwVWtHRFJNSzdacmNSam5Ucg==';
+  static const thingsboardAndroidAppSecret = 'Rm80NjMyQmhYbmZrVkJZR2R4eWozZ1BRRlBzcllRYnlOTllYcHd2eGU1bkhGY243dEVkNk5jcVJSNDNzbXpxaQ==';
+}
